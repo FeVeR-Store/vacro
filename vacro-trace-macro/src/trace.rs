3886eccd910086a1dded7398aaa4374c871c44c8
FeVeR-Store <renyis@foxmail.com>
2025-12-18 16:52:03 +0800

    feat: trace macro impl

---
+++ vacro-trace-macro/src/trace.rs
@@ -0,0 +1,58 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{
+    ItemFn, Macro, parse_quote, parse_quote_spanned,
+    visit_mut::{self, VisitMut},
+};
+
+pub fn trace_impl(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut f: ItemFn = parse_quote!(#item);
+
+    // 改写函数体里的宏调用
+    let mut rewriter = TraceRewriter::new();
+    rewriter.visit_item_fn_mut(&mut f);
+
+    quote!(#f)
+}
+
+#[allow(dead_code)]
+struct TraceRewriter {
+    rewrite_quote: bool,
+    rewrite_parse_quote: bool,
+}
+
+impl TraceRewriter {
+    fn new() -> Self {
+        Self {
+            rewrite_quote: true,
+            rewrite_parse_quote: true,
+        }
+    }
+
+    fn rewrite_macro_path(&self, mac: &mut Macro) {
+        let last_segment = mac.path.segments.last();
+        let Some(last) = last_segment else { return };
+        let last_ident_str = last.ident.to_string();
+
+        if self.rewrite_parse_quote && last_ident_str == "parse_quote" {
+            let origin_span = last.ident.span();
+
+            mac.path = parse_quote_spanned! {origin_span=>
+                ::vacro_trace_macro::parse_quote
+            };
+        }
+    }
+}
+
+impl VisitMut for TraceRewriter {
+    fn visit_macro_mut(&mut self, mac: &mut Macro) {
+        visit_mut::visit_macro_mut(self, mac);
+
+        self.rewrite_macro_path(mac);
+    }
+}
+
+pub fn parse_quote_impl(input: TokenStream) -> TokenStream {
+    let input: TokenStream = input.into();
+    quote! {::vacro_trace::parse_quote_traced(::quote::quote! {#input})}.into()
+}
