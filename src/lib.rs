7c64c27fd8d8d5803760a811bcdd9d739aadbc49
FeVeR-Store <renyis@foxmail.com>
2025-12-24 13:55:43 +0800

    refactor: extract vacro-report
    Refs #22

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,11 @@ pub mod parser {
     pub use vacro_parser::define;
 }
 
+#[cfg(feature = "report")]
+pub mod report {
+    pub use vacro_report::__private;
+    pub use vacro_report::scope;
+}
+
 #[cfg(feature = "parser")]
 pub use parser::*;
