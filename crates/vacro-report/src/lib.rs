3bec8725d1e06fda02e225ce5ef768c2c0e44f6c
FeVeR-Store <renyis@foxmail.com>
2025-12-29 00:11:28 +0800

    doc: add doc for vacro-report
    Refs #14

--- crates/vacro-report/src/lib.rs
+++ crates/vacro-report/src/lib.rs
@@ -1,3 +1,14 @@
+//! <div class="doc-en">
+//!
+#![doc = include_str!("docs/en.md")]
+//! </div>
+//!
+//! <div class="doc-cn">
+//!
+#![doc = include_str!("docs/zh_cn.md")]
+//!
+//! </div>
+
 #[doc(hidden)]
 pub mod __private;
 
